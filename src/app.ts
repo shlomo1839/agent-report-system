95338c1f3b50e40494d13b8b4c305b02020933c9
shlomo1839@gmail.com <shlomoelias1830>
2026-03-08 17:11:24 +0200

    function utils - atbash

--- src/app.ts
+++ src/app.ts
@@ -7,5 +7,4 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-
 export default app;
---
+++ src/utils/atbash.ts
@@ -0,0 +1,21 @@
+function atbash(char: string) {
+    const alphabetLowercase = "abcdefghijklmnopqrstuvwxyz"
+    const alphabetUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    return char.split("").map(char => {
+        if (alphabetLowercase.includes(char)) {
+            const index = alphabetLowercase.indexOf(char)
+            return alphabetLowercase[25 - index]
+        }
+
+        if (alphabetUppercase.includes(char)) {
+            const index = alphabetUppercase.indexOf(char)
+            return alphabetUppercase[25 - index]
+        }
+
+        return char
+    }).join("");
+}
+
+
+export default atbash;
