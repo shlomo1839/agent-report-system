634349895fc6cf523690c2530644349000629498
shlomo1839@gmail.com <shlomoelias1830>
2026-03-09 18:02:06 +0200

    chanc: controller remove to route

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,10 +1,26 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import jwt from 'jsonwebtoken';
+import {authenticateToken} from '../middleware/auth.js'
 import {login, me} from '../controllers/autController.js';
+import "dotenv/config";
 
-const router = express.Router();
+export const authRouth = express.Router();
+const secret = process.env.secret as string;
 
-router.post('/login', login)   
-router.get('/me', me)       // create middleware for token
+router.post('/login', login)
+
+
+
+
+authRouth.get('/me', authenticateToken, (req: Request, res: Response) => {
+    if (req.user) {
+        const { userId, agentCode, role } = req.user;
+        res.status(200).json({user: {userId, agentCode, role}})
+    }
+})
 
 
 export default router;
+
+
+
