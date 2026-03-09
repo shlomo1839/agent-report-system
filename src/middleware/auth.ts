bf0e9f0b809125ea352eae9dc19eb20243f9f114
shlomo1839@gmail.com <shlomoelias1830>
2026-03-09 15:16:30 +0200

    stop to countniue

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
+import { AuthPayload } from "../types/types.js";
+
+export interface AuthRequest 
 
 
-export const authenticateToken = ()
+
+export const authenticateToken = (
+
+)
